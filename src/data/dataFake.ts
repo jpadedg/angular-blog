1d818c6bb5d06ce6e4977a36ecad4e0586fdb7b3
Aded <joaopedro_aded@hotmail.com>
2022-11-17 14:43:12 -0300

    finish project

---
+++ src/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover": "https://noticias.maisesports.com.br/wp-content/uploads/2019/09/Campeonato-Brasileiro-League-of-Legends-LoL.jpg",
+    "title": "CAMPEONATO BRASILEIRO DE LOL GANHA POPULARIDADE",
+    "description": "O Campeonato Brasileiro de League of Legends é a principal competição do jogo eletrônico League of Legends no Brasil, administrada por sua desenvolvedora, Riot Games."
+  },
+  {
+    "id":"2",
+    "photoCover": "https://tecnoblog.net/wp-content/uploads/2019/05/league-of-legends.jpg",
+    "title": "League Of Legends volta a crescer entre os jovens.",
+    "description": "Olha que maneiro"
+  },
+  {
+    "id":"3",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwPtg6daL9PrdDkfZL5xaty6HevRTy_-tN5g&usqp=CAU",
+    "title": "K'sant é revelado como o novo heroi de LOL.",
+    "description": "Novo personagem de League of Legends foi lançado"
+  },
+  {
+    "id":"4",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXKK6hyouNKbv2lMQrCAGO0EKblvS5hqyDKA&usqp=CAU",
+    "title": "A LOUD foi a equipe campeão do 2° split do CBLOL 2022",
+    "description": "GG LOUD <3"
+  }
+
+]
